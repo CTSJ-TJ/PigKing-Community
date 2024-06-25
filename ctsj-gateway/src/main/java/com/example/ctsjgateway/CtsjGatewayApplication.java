3780804e108a7da2285c98ff8a375fb4217e53ad
CTSJ <my_email@domain.com>
2024-06-25 15:29:13 +0800

    配置网关

---
+++ ctsj-gateway/src/main/java/com/example/ctsjgateway/CtsjGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.ctsjgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtsjGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CtsjGatewayApplication.class, args);
+	}
+
+}
